163ace13eada0dcfc9bbdc7d333d337a375ca419
Aditya-Mishra0 <168905923+Aditya-Mishra0@users.noreply.github.com>
2025-05-05 03:55:46 +0530

    Add files via upload

---
+++ src/WordCounter.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class WordCounter {
+    public static void main(String[] args) {
+
+        JFrame jFrame = new JFrame("Word Counter") ;
+        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+        jFrame.setLayout(null);
+        jFrame.setBounds(100,100,500,500);
+
+        JLabel label = new JLabel("Enter Words ") ;
+        label.setFont(new Font("Times New Roman", Font.BOLD ,25));
+        label.setBounds(50,20,150,25);
+
+        JTextArea textArea = new JTextArea() ;
+        textArea.setBounds(50,50,375,200);
+        textArea.setFont(new Font("Serif",Font.PLAIN,18 ));
+
+        JButton button = new JButton("Count") ;
+        button.setBounds(345,265,80,40);
+
+        button.addActionListener(e -> {
+        String text = textArea.getText().trim() ;
+
+        if(!text.isEmpty()){
+            String [] word = text.trim().split("\\s+") ;
+            JOptionPane.showMessageDialog(jFrame,"Total Words: " +word.length);
+        }
+        else{
+            JOptionPane.showMessageDialog(jFrame,"Please Enter Text. ","Warning",JOptionPane.WARNING_MESSAGE);
+        }
+        });
+
+        jFrame.add(label) ;
+        jFrame.add(textArea) ;
+        jFrame.add(button) ;
+
+        jFrame.setVisible(true);
+
+    }
+}
